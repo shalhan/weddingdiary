b2fb6cb95afc728f50f034517e90fda72ecbed7c
alif16syafri <alif16syafri@hotmail.com>
2019-04-26 00:10:36 +0700

    feat: add wedding list indexing

--- resources/views/pages/wedding/index.blade.php
+++ resources/views/pages/wedding/index.blade.php
@@ -13,9 +13,62 @@
     <li><a href="../../html/.html">home</a></li>
     <li class="active">Dashboard</li>
 </ol>
-<div class="section-header">
-    <h3 class="text-standard"><i class="fa fa-fw fa-arrow-circle-right text-gray-light"></i> Index wedding <small>Handy when you need to create a new page</small></h3>
+
+<div class="section-header u-md-flex u-md-flexJustifyContentSpaceBetween u-md-flexAlignItemsCenter u-marginTop18 u-marginBottom9">
+    <h3 class="text-standard u-margin0">List of Weddings</h3>
+    <a href="/weddings/create?step=1">
+        <button type="button" class="btn btn-inverse u-marginTop16 u-md-marginTop0">Create New Wedding</button>
+    </a>
 </div>
+
 <div class="section-body">
-</div><!--end .section-body -->
+    <div class="row">
+        <div class="col-lg-12">
+
+            <div class="box">
+
+                <div class="box-head">
+                    <header>
+                        <h4 class="text-light">Table <strong>Basic</strong></h4>
+                    </header>
+                </div>
+
+                <div class="box-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Groom Name</th>
+                                <th>Bride Name</th>
+                                <th>Template</th>
+                                <th class="text-right1" style="width:90px">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                <td>
+                                    <button type="button" class="btn btn-xs btn-inverse btn-equal" data-toggle="tooltip" data-placement="top" data-original-title="Edit row"><i class="fa fa-pencil"></i></button>
+                                    <button type="button" class="btn btn-xs btn-danger btn-equal" data-toggle="tooltip" data-placement="top" data-original-title="Delete row"><i class="fa fa-trash-o"></i></button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                                <td>
+                                    <button type="button" class="btn btn-xs btn-inverse btn-equal" data-toggle="tooltip" data-placement="top" data-original-title="Edit row"><i class="fa fa-pencil"></i></button>
+                                    <button type="button" class="btn btn-xs btn-danger btn-equal" data-toggle="tooltip" data-placement="top" data-original-title="Delete row"><i class="fa fa-trash-o"></i></button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
 @endsection
