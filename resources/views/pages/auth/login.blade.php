86bbb04f3d26bcb4d882bc77654a5edb376884ad
alif16syafri <alif16syafri@hotmail.com>
2019-04-25 18:51:59 +0700

    feat: add login screen

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,6 +1,29 @@
 @extends('layouts.blank')
 
 @section('content')
-<h1>LOGIN</h1>
+<div class="row u-marginTop64">
+  <div class="col-md-4 col-md-offset-4">
+
+    <form role="form" method="POST">
+      <div class="box">
+        <div class="box-body">
+          <div class="form-vertical">
+            <div class="form-group">
+              <label for="EMAIL">Email Address</label>
+              <input type="email" name="EMAIL" id="EMAIL" class="form-control" placeholder="Your email address" value={{ old('EMAIL')}}>
+            </div>
+            <div class="form-vertical">
+              <div class="form-group">
+                <label for="PASSWORD">Password</label>
+                <input type="password" name="PASSWORD" id="PASSWORD" class="form-control" placeholder="Your password" value={{ old('PASSWORD')}}>
+              </div>
+            </div>
+          </div>
+          <button type="submit" class="btn btn-inverse u-fullWidth">Login</button>
+        </div>
+    </form>
+
+  </div>
+</div>
 
 @endsection
