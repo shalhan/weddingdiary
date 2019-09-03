175c083a3d93927cedf6a106129bbde08034e4c9
shalhan <shalhanradifan44@gmail.com>
2019-09-03 10:41:54 +0700

    change trmessage charset into uft8mb4

---
+++ database/migrations/2019_09_03_034028_change_table_trmessage_charset_into_uft8mb4.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeTableTrmessageCharsetIntoUft8mb4 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('ALTER TABLE `trmessage` CONVERT TO CHARACTER SET utf8mb4');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('ALTER TABLE `trmessage` CONVERT TO CHARACTER SET utf8');
+    }
+}
